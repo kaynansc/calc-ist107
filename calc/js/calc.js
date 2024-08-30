029ed61f0e6740c9e59f6686af9f385be60827ef
kaynan <kaynan.camargo@terceiro-sky.com.br>
2024-08-29 20:16:05 -0700

    test my new name

--- calc/js/calc.js
+++ calc/js/calc.js
@@ -1,6 +1,6 @@
 "use strict";
 
-document.getElementById("nameId").textContent = "Test my new name"
+document.getElementById("nameId").textContent = "Kaynan | Test push via github action"
 
 //Dynamic background color change
 const color = ['#F0E68C', '#FFDAB9', '#FFE4B5', '#D8BFD8', '#B0E0E6', '#AFEEEE', '#E0FFFF', '#98FB98', '#FFDEAD', '#F5DEB3'];
